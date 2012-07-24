2f8d7492be54ea20b5899d0611006c0c1fad8927
Den <den@den-laptop.(none)>
2012-07-24 13:22:05 +0400

    registration

--- src/Project/DataBundle/Controller/DefaultController.php
+++ src/Project/DataBundle/Controller/DefaultController.php
@@ -27,29 +27,23 @@ class DefaultController extends Controller
 			$request->getPathInfo();		
 			$name = $request->request->get('_username');
 			$pass = $request->request->get('_password');
-					
+			// is Empty
+			if (($pass == '')||($name == '') ) return array('name'=>'Fill the gaps');		
+			
 			$a = new FormCheck();
-			//file
-			
-			$a->test();
-			//$request = $this->getRequest();
 			$user = new User();
-			
-			if(isset($_FILE['filename']))
-				$user->setImg($a->uploadFile());
+			//file
+			if($_FILES['filename']['name'] != '')
+				{$ka = $a->uploadFile(); $user->setImg($ka);}
 			else{ $user->setImg(getcwd().'/letsdance/static/files/'.'default.png'); }
 			 
 			$user->setUsername($name);
 			
 			$factory = $this->get('security.encoder_factory');
-			
 			$encoder = $factory->getEncoder($user);
 			$password = $encoder->encodePassword($pass, $user->getSalt());
 			$user->setPassword($password);
-			
 			$user->setRoles(array('ROLE_USER'));
-			
-			echo($user->getUsername());
 			$em = $this->getDoctrine()->getEntityManager();
 			$em->persist($user);
 			$em->flush();
